e17c5ffd4f7878a5a0041972328bb9469e2fa301
Tanxy Rogue <tanxyrogue@gmail.com>
2019-11-09 09:22:29 +0200

    added sendUserMessage function to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,36 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+var transporter = nodemailer.createTransport({
+    service : 'Gmail',
+    auth: {
+        user: 'info.letsgodigital@gmail.com',
+        pass: 'letsgo2019'
+    }
+});
+
+var mailOptions = {
+    from: 'info.letsgodigital@gmail.com',
+    to: 'info.letsgodigital@gmail.com',
+    subject: 'New Message Let on Website'
+};
+
+exports.sendUserMessage = functions
+    .firestore
+    .document('messsages/{messsagesID}')
+    .onCreate((snapshot, context)=>{
+        // mailOptions.text = "A new Booking has been made by <br>" + snapshot.data().name + " for the <br/>" + snapshot.data().service + " service, on " + snapshot.data().date + " during the " + snapshot.data().timeslot + ". /nHis/Her phone number is " + snapshot.data().phone;
+        mailOptions.html = `A new Message has been left by a user named: <strong>${snapshot.data().UserName}</strong><br/>
+                            Their Phone number is: <strong>${snapshot.data().UserPhone}</strong>, Their email is: <strong>${snapshot.data().UserEmail}.</strong><br/>
+                            <br/>
+                            Their message is: <em><b>${snapshot.data().Message}</b></em>`;
+
+        return transporter.sendMail(mailOptions)
+        .then(()=>{
+            console.log("New User Message email sent to admin");
+            return null;
+        })
+    })
